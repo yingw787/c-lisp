945af362df109e3e952663b3260dd7ff0d50ab1c
Ying Wang <ywang@xometry.com>
2017-04-18 11:54:14 -0400

    Add chapter_9 evaluation of parsed S-expression.

--- chapter_9/s_expressions.c
+++ chapter_9/s_expressions.c
@@ -47,6 +47,7 @@ enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 /* Declare functions */
 void lval_print(lval* v);
+lval* lval_eval(lval* v);
 
 /* Construct a pointer to a new Number lval */
 lval* lval_num(long x) {
@@ -176,52 +177,106 @@ void lval_println(lval* v) {
     putchar('\n');
 }
 
-// /* Use operator string to see which operation to perform */
-// lval eval_op(lval x, char* op, lval y) {
-//
-//     /* If either value is an error return it */
-//     if (x.type == LVAL_ERR) { return x; }
-//     if (y.type == LVAL_ERR) { return y; }
-//
-//     /* Otherwise do maths on the number values */
-//     if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
-//     if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
-//     if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
-//     if (strcmp(op, "/") == 0) {
-//         /* If second operand is zero return error */
-//         return y.num == 0
-//             ? lval_err(LERR_DIV_ZERO)
-//             : lval_num(x.num / y.num);
-//     }
-//
-//     return lval_err(LERR_BAD_OP);
-// }
-//
-// lval eval(mpc_ast_t* t) {
-//
-//     /* If tagged as number return it directly. */
-//     if (strstr(t->tag, "number")) {
-//         /* Check if there is some error in conversion */
-//         errno = 0;
-//         long x = strtol(t->contents, NULL, 10);
-//         return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-//     }
-//
-//     /* The operator is always second child. */
-//     char* op = t->children[1]->contents;
-//
-//     /* We store the third child in `x` */
-//     lval x = eval(t->children[2]);
-//
-//     /* Iterate the remaining children and combining. */
-//     int i = 3;
-//     while (strstr(t->children[i]->tag, "expr")) {
-//         x = eval_op(x, op, eval(t->children[i]));
-//         i++;
-//     }
-//
-//     return x;
-// }
+lval* lval_pop(lval* v, int i) {
+    /* Find the item at "i" */
+    lval* x = v->cell[i];
+
+    /* Shift memory after the item at "i" over the top */
+    memmove(&v->cell[i], &v->cell[i+1],
+        sizeof(lval*) * (v->count-i-1));
+
+    /* Decrease the count of items in the list */
+    v->count--;
+
+    /* Reallocate the memory used */
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    return x;
+}
+
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
+    return x;
+}
+
+lval* builtin_op(lval* a, char* op) {
+
+    /* Ensure all arguments are numbers */
+    for (int i = 0; i < a->count; i++) {
+        if (a->cell[i]->type != LVAL_NUM) {
+            lval_del(a);
+            return lval_err("Cannot operate on non-number!");
+        }
+    }
+
+    /* Pop the first element */
+    lval* x = lval_pop(a, 0);
+
+    /* If no arguments and sub then perform unary negation */
+    if ((strcmp(op, "-") == 0) && a->count == 0) {
+        x->num = -x->num;
+    }
+
+    /* WHile there are still elements remaining */
+    while(a->count > 0) {
+
+        /* Pop the next element */
+        lval* y = lval_pop(a, 0);
+
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "/") == 0) {
+            if (y->num == 0) {
+                lval_del(x); lval_del(y);
+                x = lval_err("Division By Zero!"); break;
+            }
+            x->num /= y->num;
+        }
+
+        lval_del(y);
+    }
+
+    lval_del(a); return x;
+}
+
+lval* lval_eval_sexpr(lval* v) {
+
+    /* Evaluate Children */
+    for (int i = 0; i < v->count; i++) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    /* Error checking */
+    for (int i = 0; i < v->count; i++) {
+        if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+    }
+
+    /* Empty Expression */
+    if (v->count == 0) { return v; }
+
+    /* Single Expression */
+    if (v->count == 1) { return lval_take(v, 0); }
+
+    /* Ensure First Element is Symbol */
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f); lval_del(v);
+        return lval_err("S-expression Does not start with symbol!");
+    }
+
+    /* Call builtin with operator */
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+    return result;
+}
+
+lval* lval_eval(lval* v) {
+    /* Evaluate Sexpressions */
+    if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+    /* ALl other lval types remain the same */
+    return v;
+}
 
 int main(int argc, char** argv) {
 
@@ -260,7 +315,7 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
             /* On Success Print the Result */
-            lval* x = lval_read(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
         } else {
