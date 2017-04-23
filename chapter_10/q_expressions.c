aa7321f26cbac2848fbd11af689e52979c1c60c9
Ying Wang <yingw787@gmail.com>
2017-04-22 20:48:42 -0400

    Add LASSERT macro, edited head and tail builtin functions, added rest of functions, and added builtin function and updated lval_eval_sexpr function.

--- chapter_10/q_expressions.c
+++ chapter_10/q_expressions.c
@@ -48,6 +48,7 @@ enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 /* Declare functions */
 void lval_print(lval* v);
 lval* lval_eval(lval* v);
+lval* builtin(lval* a, char* func);
 
 /* Construct a pointer to a new Number lval */
 lval* lval_num(long x) {
@@ -280,7 +281,7 @@ lval* lval_eval_sexpr(lval* v) {
     }
 
     /* Call builtin with operator */
-    lval* result = builtin_op(v, f->sym);
+    lval* result = builtin(v, f->sym);
     lval_del(f);
     return result;
 }
@@ -292,22 +293,14 @@ lval* lval_eval(lval* v) {
     return v;
 }
 
+#define LASSERT(args, cond, err) \
+    if (!(cond)) { lval_del(args); return lval_err(err); }
+
 lval* builtin_head(lval* a) {
     /* Check Error Conditions */
-    if (a->count != 1) {
-        lval_del(a);
-        return lval_err("Function 'head' passed too many arguments!");
-    }
-
-    if (a->cell[0]->type != LVAL_QEXPR) {
-        lval_del(a);
-        return lval_err("Function 'head' passed incorrect types!");
-    }
-
-    if (a->cell[0]->count == 0) {
-        lval_del(a);
-        return lval_err("Function 'head' passed {}!");
-    }
+    LASSERT(a, a->count == 1, "Function 'head' passed too many arguments!");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect type!");
+    LASSERT(a, a->cell[0]->count != 0, "Function 'head' passed {}!");
 
     /* Otherwise take first argument */
     lval* v = lval_take(a, 0);
@@ -319,19 +312,9 @@ lval* builtin_head(lval* a) {
 
 lval* builtin_tail(lval* a) {
     /* Check Error Conditions */
-    if (a->count != 1) {
-        return lval_err("Function 'tail' passed too many arguments!");
-    }
-
-    if (a->cell[0]->type != LVAL_QEXPR) {
-        lval_del(a);
-        return lval_err("Function 'tail' passed incorrect types!");
-    }
-
-    if (a->cell[0]->count == 0) {
-        lval_del(a);
-        return lval_err("Function 'tail' passed {}!");
-    }
+    LASSERT(a, a->count ==1, "Function 'tail' passed too many arguments!");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect type!");
+    LASSERT(a, a->cell[0]->count != 0, "Function 'head' passed {}!");
 
     /* Take first argument */
     lval* v = lval_take(a, 0);
@@ -339,6 +322,59 @@ lval* builtin_tail(lval* a) {
     /* Delete first element and return */
     lval_del(lval_pop(v, 0));
     return v;
+}
+
+lval* builtin_list(lval* a) {
+    a->type = LVAL_QEXPR;
+    return a;
+}
+
+lval* builtin_eval(lval* a) {
+    LASSERT(a, a->count == 1, "Function 'eval' passed too many arguments!");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'eval' passed incorrect type!");
+
+    lval* x = lval_take(a, 0);
+    x->type = LVAL_SEXPR;
+    return lval_eval(x);
+}
+
+lval* lval_join(lval* x, lval* y) {
+
+    /* For each cell in 'y' add it to 'x' */
+    while (y->count) {
+        x = lval_add(x, lval_pop(y, 0));
+    }
+
+    /* Delete the empty 'y' and return 'x' */
+    lval_del(y);
+    return x;
+}
+
+lval* builtin_join(lval* a) {
+
+    for (int i = 0; i < a->count; i++) {
+        LASSERT(a, a->cell[i]->type == LVAL_QEXPR, "Function 'join' passed incorrect type.");
+    }
+
+    lval* x = lval_pop(a, 0);
+
+    while (a->count) {
+        x = lval_join(x, lval_pop(a, 0));
+    }
+
+    lval_del(a);
+    return x;
+}
+
+lval* builtin(lval* a, char* func) {
+    if (strcmp("list", func) == 0) { return builtin_list(a); }
+    if (strcmp("head", func) == 0) { return builtin_head(a); }
+    if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+    if (strcmp("join", func) == 0) { return builtin_join(a); }
+    if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+    if (strcmp("+-/*", func)) { return builtin_op(a, func); }
+    lval_del(a);
+    return lval_err("Unknown Function!");
 }
 
 int main(int argc, char** argv) {
