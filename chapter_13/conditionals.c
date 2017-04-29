c2ad079b29e6f9bbbf003bfaa7927bbbaa124a7e
Ying Wang <yingw787@gmail.com>
2017-04-29 18:44:23 -0400

    Add working equality operators builtin_eq, builtin_ne

--- chapter_13/conditionals.c
+++ chapter_13/conditionals.c
@@ -699,6 +699,69 @@ lval* builtin_le(lenv* e, lval* a) {
     return builtin_ord(e, a, "<=");
 }
 
+int lval_eq(lval* x, lval* y) {
+  /* Different Types are always unequal */
+  if (x->type != y->type) {
+    return 0;
+  }
+
+  /* Compare Based upon type */
+  switch(x->type) {
+    /* Compare Number Value */
+    case LVAL_NUM:
+      return (x->num == y->num);
+    /* Compare String Values */
+    case LVAL_ERR:
+      return (strcmp(x->err, y->err) == 0);
+    case LVAL_SYM:
+      return (strcmp(x->sym, y->sym) == 0);
+    /* If builtin compare, otherwise compare formals and body */
+    case LVAL_FUN:
+      if (x->builtin || y->builtin) {
+        return x->builtin == y->builtin;
+      } else {
+        return lval_eq(x->formals, y->formals) && lval_eq(x->body, y->body);
+      }
+    /* If list compare every individual element */
+    case LVAL_QEXPR:
+    case LVAL_SEXPR:
+      if (x->count != y->count) {
+        return 0;
+      }
+      for (int i = 0; i < x->count; i++) {
+        /* If any element not equal then whole list not equal */
+        if (!lval_eq(x->cell[i], y->cell[i])) {
+          return 0;
+        }
+      }
+      /* Otherwise lists must be equal */
+      return 1;
+    break;
+  }
+  return 0;
+}
+
+lval* builtin_cmp(lenv* e, lval* a, char* op) {
+  LASSERT_NUM(op, a, 2);
+  int r;
+  if (strcmp(op, "==") == 0) {
+    r = lval_eq(a->cell[0], a->cell[1]);
+  }
+  if (strcmp(op, "!=") == 0) {
+    r = !lval_eq(a->cell[0], a->cell[1]);
+  }
+  lval_del(a);
+  return lval_num(r);
+}
+
+lval* builtin_eq(lenv* e, lval* a) {
+  return builtin_cmp(e, a, "==");
+}
+
+lval* builtin_ne(lenv* e, lval* a) {
+  return builtin_cmp(e, a, "!=");
+}
+
 void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
   lval* k = lval_sym(name);
   lval* v = lval_fun(func);
@@ -730,6 +793,8 @@ void lenv_add_builtins(lenv* e) {
   lenv_add_builtin(e, ">", builtin_gt);
   lenv_add_builtin(e, "<=", builtin_le);
   lenv_add_builtin(e, ">=", builtin_ge);
+  lenv_add_builtin(e, "==", builtin_eq);
+  lenv_add_builtin(e, "!=", builtin_ne);
 }
 
 /* Evaluation */
